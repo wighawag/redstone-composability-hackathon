5908bff73356cf3c94d6d949bb937375d97d9fae
wighawag <wighawag@gmail.com>
2024-05-12 13:02:46 +0100

    use vote round system

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -3,6 +3,24 @@ import { defineWorld } from "@latticexyz/world";
 export default defineWorld({
   namespace: 'voteforbot',
   tables: {
+    Vote0: {
+      key: ["matchEntity"],
+      schema: {
+        matchEntity: "bytes32",
+        voteForPlayer1: "uint8",
+        voteForPlayer2: "uint8",
+        voteForPlayer3: "uint8",
+      },
+    },
+    Vote1: {
+      key: ["matchEntity"],
+      schema: {
+        matchEntity: "bytes32",
+        voteForPlayer1: "uint8",
+        voteForPlayer2: "uint8",
+        voteForPlayer3: "uint8",
+      },
+    },
     BotMatch: {
       key: ["matchEntity"],
       schema: {
--- packages/contracts/src/systems/BotActionSystem.sol
+++ packages/contracts/src/systems/BotActionSystem.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.24;
 
 import { System } from "@latticexyz/world/src/System.sol";
-import {BotMatch, BotMatchData} from '../codegen/index.sol';
+import {BotMatch, BotMatchData, Vote0Data, Vote0, Vote1Data, Vote1} from '../codegen/index.sol';
 import {PositionData, Position, EntitiesAtPosition, Untraversable, Factory} from '../skystrife/codegen/index.sol';
 import {IWorld} from '../skystrife/codegen/world/IWorld.sol';
 import { playerFromAddress, matchHasStarted } from "../skystrife/libraries/LibUtils.sol";
@@ -10,6 +10,11 @@ import {LibGold} from '../skystrife/libraries/LibGold.sol';
 
 contract BotActionSystem is System {
 
+ struct VoteData {
+    uint8 player1Votes;
+    uint8 player2Votes;
+    uint8 player3Votes;
+ }
 
   function joinMatch(bytes32 matchEntity) external {    
     // join the match
@@ -28,16 +33,54 @@ contract BotActionSystem is System {
   }
 
 
+    // no way to get list of enemy units onchain ?
+    // passing units list work but allow bot runner to affect which factory get used  
   function process(bytes32 matchEntity) external {
     IWorld world = IWorld(_world());
     BotMatchData memory matchInfo = BotMatch.get(matchEntity);
 
+    (bool foundTarget, bytes32 targetPlayer) = _getCurrentTarget(matchEntity);
+
     _processFactories(world, matchEntity, matchInfo.factories);
-    _processUnits(world, matchEntity, matchInfo.units);
+    if (foundTarget) {
+        _processUnits(world, matchEntity, matchInfo.units, targetPlayer);
+    }
+    
   }
 
 
-  function _processUnits(IWorld world, bytes32 matchEntity, bytes32[] memory unitEntities) internal {
+    function _getCurrentTarget(bytes32 matchEntity) internal view returns (bool found, bytes32 playerTarget) {
+        
+        VoteData memory voteData = _getCurrentVote(matchEntity);
+        if (voteData.player1Votes > voteData.player2Votes && voteData.player1Votes > voteData.player3Votes) {
+            found = true;
+            playerTarget = 0x0000000000000000000000000000000000000000000000000000000000000001; // TODO
+        } else if (voteData.player2Votes > voteData.player1Votes && voteData.player2Votes > voteData.player3Votes) {
+            found = true;
+            playerTarget = 0x0000000000000000000000000000000000000000000000000000000000000002; // TODO
+        } else if (voteData.player3Votes > voteData.player1Votes && voteData.player3Votes > voteData.player2Votes) {
+            found = true;
+            playerTarget = 0x0000000000000000000000000000000000000000000000000000000000000003; // TODO
+        }
+    }
+
+  function _getCurrentVote(bytes32 matchEntity) internal view returns (VoteData memory voteData) {
+    uint256 round = (block.timestamp / 60) % 2;
+    if (round == 0) {
+        Vote0Data memory v = Vote0.get(matchEntity);
+        voteData.player1Votes = v.voteForPlayer1;
+        voteData.player2Votes = v.voteForPlayer2;
+        voteData.player3Votes = v.voteForPlayer3;
+    } else {
+        Vote1Data memory v = Vote1.get(matchEntity);
+        voteData.player1Votes = v.voteForPlayer1;
+        voteData.player2Votes = v.voteForPlayer2;
+        voteData.player3Votes = v.voteForPlayer3;
+    }
+  }
+
+
+  function _processUnits(IWorld world, bytes32 matchEntity, bytes32[] memory unitEntities, bytes32 targetPlayer) internal {
     // define order ?
     for (uint256 i =0; i < unitEntities.length; i++) {
         _processUnit(world, matchEntity, unitEntities[i]);
